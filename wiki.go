ffefd753b5a7c3a71e1be39d14c046ed7b59eca9
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-07-03 18:08:02 -0400

    basic working site

--- wiki.go
+++ wiki.go
@@ -1,37 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
+	"net/http"
 )
 
-/*
-	Describes how pages will be stores in MEMORY
-	Not how they will be in storages
-*/
 type Page struct {
 	Title string
-	Body  []byte // io expects byte not strings
+	Body  []byte
 }
 
-/*
-	Method named save that takes a page and saves it
-	Will return an error if anything goes wrong but nil if everything goes right
-*/
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
-	/*
-		The 0600 octal integer lets the program know that the file
-		written should only be given read-write permissions for the
-		current user only
-	*/
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-/*
-	Loads the page file from the title param
-	Returns a pointer to the page literal constructed from the file
-*/
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
@@ -41,9 +25,45 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusNotFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
 }
